3a8d9297dfcde13288e7c790cc95e48dd4659c95
isaac.shapira <isaac.shapira@logrhythm.com>
2015-03-05 10:59:52 -0700

    broke

--- src/Control/RequestAnimationFrame.purs
+++ src/Control/RequestAnimationFrame.purs
@@ -1,26 +1,37 @@
 module Control.RAF where
 
 import Control.Monad.Eff
+import Context
 
 -- http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
 
 foreign import data RAF :: !
 
-foreign import requestAnimationFrame """
-  
-  var rAF = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            function( callback ){
-              window.setTimeout(callback, 1000 / 60);
-            };
-  })();
+foreign import requestAnimationFrame_ """
+  var rAF = null;
 
-  function requestAnimationFrame(x){
-    return function(){ 
-      return rAF(x); 
-    };
+  function requestAnimationFrame_(context){
+    return function(x){
+
+      if(!rAF){
+        rAF = (function(){
+          return  context.requestAnimationFrame       ||
+                  context.webkitRequestAnimationFrame ||
+                  context.mozRequestAnimationFrame    ||
+                  function( callback ){
+                    context.setTimeout(callback, 1000 / 60);
+                  };
+        })();
+      }
+
+      return function(){
+        return rAF(x);
+      };
+
+    }
   };
-  
-""" :: forall a e. Eff (raf :: RAF | e) a -> Eff (raf :: RAF | e) Unit
+
+""" :: forall a e. Context -> Eff (raf :: RAF | e) a -> Eff (raf :: RAF | e) Unit
+
+requestAnimationFrame :: forall a e. Eff (raf :: RAF | e) a -> Eff (raf :: RAF | e) Unit
+requestAnimationFrame = getContext >>= requestAnimationFrame_
